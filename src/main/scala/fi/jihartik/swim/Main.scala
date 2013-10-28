aca1f3888cbf909cbbc7ef0f33ee2907a99bb617
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2013-10-28 19:22:10 +0200

    Use correct amount of nodes (before 1 too many)

--- src/main/scala/fi/jihartik/swim/Main.scala
+++ src/main/scala/fi/jihartik/swim/Main.scala
@@ -30,7 +30,7 @@ object Node3 extends App {
 object MultiNode extends App {
   val system = ActorSystem()
   val max = args(0).toInt
-  (20 to 20 + max).par.foreach { i =>
+  (20 until 20 + max).par.foreach { i =>
     val node = system.actorOf(Props(classOf[Node], s"127.0.0.$i", 10000 + i))
     Thread.sleep(500)
     node ! Join(new InetSocketAddress("127.0.0.1", 10001))
