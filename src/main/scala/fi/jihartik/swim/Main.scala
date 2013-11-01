d2e9f948f194e56c45c2c623a44e3cc9caf0b8e7
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2013-11-01 20:56:38 +0200

    Fix constructors of test main classes

--- src/main/scala/fi/jihartik/swim/Main.scala
+++ src/main/scala/fi/jihartik/swim/Main.scala
@@ -6,19 +6,19 @@ import java.net.InetSocketAddress
 
 object Node1 extends App {
   val system = ActorSystem()
-  system.actorOf(Props(classOf[Node], "127.0.0.1", 10001))
+  system.actorOf(Props(classOf[Node], "127.0.0.1", 10001, DefaultConfig))
 }
 
 object Node2 extends App {
   val system = ActorSystem()
-  val node = system.actorOf(Props(classOf[Node], "127.0.0.2", 10002))
+  val node = system.actorOf(Props(classOf[Node], "127.0.0.1", 10002, DefaultConfig))
   node ! Join("127.0.0.1", 10001)
 }
 
 
 object Node3 extends App {
   val system = ActorSystem()
-  val node = system.actorOf(Props(classOf[Node], "127.0.0.3", 10003))
+  val node = system.actorOf(Props(classOf[Node], "127.0.0.1", 10003, DefaultConfig))
   node ! Join("127.0.0.1", 10001)
 }
 
@@ -29,7 +29,7 @@ object MultiNode extends App {
   val system = ActorSystem()
   val max = args(0).toInt
   (20 until 20 + max).par.foreach { i =>
-    val node = system.actorOf(Props(classOf[Node], s"127.0.0.$i", 10000 + i))
+    val node = system.actorOf(Props(classOf[Node], s"127.0.0.$i", 10000 + i, DefaultConfig))
     Thread.sleep(500)
     node ! Join("127.0.0.1", 10001)
   }
