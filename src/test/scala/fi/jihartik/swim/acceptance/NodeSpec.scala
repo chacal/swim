dbd59d8967f82c98714d3e8cc6d878af65a93b73
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2013-11-01 22:18:40 +0200

    Add more tests for node

--- src/test/scala/fi/jihartik/swim/acceptance/NodeSpec.scala
+++ src/test/scala/fi/jihartik/swim/acceptance/NodeSpec.scala
@@ -1,12 +1,15 @@
 package fi.jihartik.swim.acceptance
 
 import fi.jihartik.swim._
-import akka.actor.ActorRef
+import akka.actor.{Props, ActorRef}
 import spray.client.pipelining._
 import fi.jihartik.swim.SuspectMember
 import fi.jihartik.swim.DeadMember
 import fi.jihartik.swim.Member
 import org.scalatest.concurrent.ScalaFutures
+import java.net.InetSocketAddress
+import akka.actor.ActorDSL._
+import akka.pattern.ask
 
 class NodeSpec extends NodeTesting with ScalaFutures {
   "Node" should {
@@ -22,6 +25,51 @@ class NodeSpec extends NodeTesting with ScalaFutures {
       val response = s"""[{"name":"Node 127.0.0.1:$port1","ip":"127.0.0.1","port":$port1,"state":"alive","incarnation":0}]"""
       httpPipeline(Get(s"http://$host:${port1}/members")).futureValue.entity.asString should be(response)
     }
+    "broadcast cluster state changes configured times" in {
+      val (_, node1) = startNode()
+      val (port2, collector) = udpMessageCollector
+
+      val aliveMessage = AliveMember(Member("TestNode", host, port2, Alive, 0))
+      node1 ! aliveMessage  // "join" our collector -> node1 should start broadcasting this
+      eventually { collectedClusterMessagesFrom(collector) should be(1.to(TestConfig.maxBroadcastTransmitCount).map(_ => aliveMessage)) }
+    }
+    "send indirect probes when direct probe times out" in {
+      // Create cluster of two nodes & collector
+      val (port1, node1) = startNode()
+      val (port2, node2) = startNode()
+      val (collectorPort, collector) = udpMessageCollector
+      node2 ! Join(host, port1)
+      node1 ! AliveMember(Member("TestNode", host, collectorPort, Alive, 0))
+      assertNotDeadClusterSize(node1 :: node2 :: Nil, 3)
+
+      node2 ! Stop
+
+      // Node 1 should try to reach (now stopped) node 2 via collector
+      eventually { collectedFailureDetectionMessagesFrom(collector).find {
+        case IndirectPing(_, Member(_, _, `port2`, Alive, 0)) => true
+        case _ => false
+      } should be('defined) }
+    }
+  }
+
+  def collectedClusterMessagesFrom(collector: ActorRef) = collector.ask("getClusterMessages").mapTo[List[ClusterStateMessage]].futureValue
+  def collectedFailureDetectionMessagesFrom(collector: ActorRef) = collector.ask("getFailureDetectionMessages").mapTo[List[FailureDetectionMessage]].futureValue
+
+  def udpMessageCollector = {
+    val port = RandomPort()
+    val udp = system.actorOf(Props(classOf[UdpComms], new InetSocketAddress(host, port)))
+    system.actorOf(Props(classOf[FailureDetector], udp, TestConfig)) // Start failure detector to respond to pings and to keep us alive in the cluster
+    val collectorActor = actor(new Act {
+      val received = new scala.collection.mutable.ListBuffer[UdpMessage]
+      become {
+        case CompoundUdpMessage(messages) => messages.foreach(self ! _)  // Unpack compound messages
+        case msg: UdpMessage =>  received += msg
+        case "getClusterMessages" => sender ! received.toList.filter(_.isInstanceOf[ClusterStateMessage])
+        case "getFailureDetectionMessages" => sender ! received.toList.filter(_.isInstanceOf[FailureDetectionMessage])
+      }
+    })
+    udp ! RegisterReceiver(collectorActor)
+    (port, collectorActor)
   }
 
   def testRefuting(reason: Member => ClusterStateMessage) {
